979e5790129ea1b5422896fe80f1042d1ff84233
TOBY PETERSON <tspeterson206@gmail.com>
2019-05-18 22:27:41 -0700

    added tutor component

--- App.js
+++ App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Main from './Main';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'
+import tutorsReducer from './reducers/tutorsReducer'
 
-const store = createStore();
+const store = createStore(tutorsReducer);
 
 export default () => (
 <Provider store={store}>
